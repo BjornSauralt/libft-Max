725cda1a7fa83754586123b735cfb16a5cb74eaf
BjornSauralt <maxime.garsault@gmail.com>
2024-09-21 12:17:01 +0200

    libft.h
    creation de libft.h + appel premiere fonction

---
+++ libft.h
@@ -0,0 +1,14 @@
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+//Partie obligatoire
+#include "stdlib.h"
+#include "unistd.h"
+#include "string.h"
+
+
+//focntions de la librairie :
+void    *ft_isalpha(char str)
+
+#endif
