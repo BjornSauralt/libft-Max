2612e6ce8bd6799fe06fa9bbdf31cc1b88ffb099
BjornSauralt <maxime.garsault@gmail.com>
2024-10-15 16:47:00 +0200

    changes

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,3 +11,26 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	ft_strnstr(char *str, char *to_find, int size)
+{
+	int	i;
+
+	if (*to_find == '\0')
+		return (str);
+	while (*str)
+	{
+		if (*str == *to_find)
+		{
+			i = 0;
+			while (to_find[i] != '\0' && str[i] == to_find[i])
+			{
+				i++;
+			}
+			if (to_find[i] == '\0')
+				return (str);
+		}
+		str++;
+	}
+	return (NULL);
+}
